f7ec20c86813f773d9b234231ea55a4249830f31
javacasm <javacasm@gmail.com>
2016-12-04 00:09:20 +0100

    v0 T01

---
+++ icezum/T01-setbit/setbit.v
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------
+//-- Verilog template
+//-- Top entity
+//-- Board: icezum
+//------------------------------------------------------------------
+`default_nettype none
+
+//-- Fichero setbit.v
+module setbit(output A);
+wire A;
+
+assign A = 1;
+
+endmodule
